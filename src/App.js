3d9ac4691a914753eb460a522d373f9b5d0edbe2
jLurd <jeylourdalcontin@gmail.com>
2023-04-11 14:22:19 +0000

    https://github.com/jeylourd/myFirstReactProject.git

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          HELLO TEST
+          HELLO TESTS
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
